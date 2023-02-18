f3d5d3a17b14f00d19bb5abbe421aa240026842f
Kaldrr <tomekmichalak11@gmail.com>
2023-02-18 18:51:23 +0100

    Added exporting database as csv using entity framework core

---
+++ AccessDatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace net_test
+{
+    internal class AccessDatabaseContext : DbContext
+    {
+        // DbSet maps directly to a table in a given database
+        // Name MUST be the same as table name, here 'Table1'
+        public DbSet<TableRecord> Table1 { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // TODO: set correct path to the database file on Azure
+            optionsBuilder.UseJetOleDb(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=./../../../test_db.accdb;Persist Security Info=True");
+        }
+    }
+}
